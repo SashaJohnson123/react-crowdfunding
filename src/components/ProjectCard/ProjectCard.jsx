5baffa5514c52d59e749fa65b952109aa6f6f34b
Sasha Johnson <johnson.sashamarie@gmail.com>
2020-09-16 13:16:01 +0800

    Finalised ProjectPage

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom"; import "./ProjectCard.css";
+import { Link } from "react-router-dom";
+import "./ProjectCard.css";
+
 function ProjectCard(props) {
-    const { projectData } = props; return (
-        <div className="project-card"​> <Link to="/project">
-            <img src={projectData.image} /> <h3>{projectData.title}</h3> </Link>
-        </div>);
+    const { projectData } = props;
+    return (
+        <div className="project-card"​>
+            <Link to="/project">
+                <img src={projectData.image} />
+                <h3>{projectData.title}</h3>
+            </Link>
+        </div>
+    );
 }
+
 export default ProjectCard;
 
 
