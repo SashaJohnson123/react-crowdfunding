2d7b083d204fd0dfc7b016615dbc5b6931bfbdd4
Sasha Johnson <johnson.sashamarie@gmail.com>
2020-09-16 13:00:43 +0800

    Styled css; Finalised files

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,22 +3,12 @@ import { allProjects } from "../data";
 import ProjectCard from "../components/ProjectCard/ProjectCard";
 function HomePage() {
     return (
-        <div>
+        <div id="project-list"​>
             {allProjects.map((projectData, key) => {
                 return <ProjectCard key={key} projectData={projectData} />;
             })}
         </div>
     );
-
-    function HomePage() {
-        return (
-            <div id="project-list"​>
-                {allProjects.map((projectData, key) => {
-                    return <ProjectCard key={key} projectData={projectData} />;
-                })}
-            </div>
-        );
-    }
-}
+};
 export default HomePage;
 
