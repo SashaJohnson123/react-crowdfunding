3fe23b6b88f814a736276b30a55d272996aa44b9
Sasha Johnson <johnson.sashamarie@gmail.com>
2020-09-25 11:48:12 +0800

    Updated Nav Bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,9 +5,19 @@ function Nav(props) {
   return (
     <div>
       <img className="Logo" src={image} alt="Logo" />
+      <div css="#img-container"></div>
 
       <div className="Menu">
         <Link to="/">Home</Link>
+      </div>
+      <div className="Menu">
+        <Link to="/">About</Link>
+      </div>
+      <div className="Menu">
+        <Link to="/">Subscribe</Link>
+      </div>
+      <div className="Menu">
+        <Link to="/">Contact</Link>
       </div>
     </div>
   );
