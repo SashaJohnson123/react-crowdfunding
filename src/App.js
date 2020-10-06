fe229e7b56940e4a8f562184602d18883507244c
Sasha Johnson <johnson.sashamarie@gmail.com>
2020-10-06 16:29:17 +0800

    Updated styling across all pages

--- src/App.js
+++ src/App.js
@@ -10,46 +10,50 @@ import Subscribe from "./pages/Subscribe";
 import Contact from "./pages/Contact";
 import CreateProject from "./pages/CreateProject";
 import Error404 from "./components/Error404/Error404";
+import Socials from "./components/Socials/Socials";
 
 export default function App() {
   return (
-    <Router>
-      <div>
-        <Nav image={require("./Pics/13.png")} />
+    <div>
+      <Router>
+        <div>
+          <Nav image={require("./Pics/13.png")} />
 
-        <Switch>
-          <Route path="/project/:id">
-            <ProjectPage />
-          </Route>
+          <Switch>
+            <Route path="/project/:id">
+              <ProjectPage />
+            </Route>
 
-          <Route path="/login">
-            <LoginPage />
-          </Route>
+            <Route path="/login">
+              <LoginPage />
+            </Route>
 
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/Home">
-            <Home />
-          </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/Home">
+              <Home />
+            </Route>
 
-          <Route path="/About">
-            <About />
-          </Route>
-          <Route path="/Subscribe">
-            <Subscribe />
-          </Route>
-          <Route path="/Contact">
-            <Contact />
-          </Route>
-          <Route path="/CreateProject">
-            <CreateProject />
-          </Route>
-          <Route>
-            <Route path="*" exact={true} component={Error404} />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+            <Route path="/About">
+              <About />
+            </Route>
+            <Route path="/Subscribe">
+              <Subscribe />
+            </Route>
+            <Route path="/Contact">
+              <Contact />
+            </Route>
+            <Route path="/CreateProject">
+              <CreateProject />
+            </Route>
+            <Route>
+              <Route path="*" exact={true} component={Error404} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+      <Socials />
+    </div>
   );
 }
--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -1,33 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { Link, useHistory, useLocation } from "react-router-dom";
+import React from "react";
+import "./Socials.css";
 
-function Socials {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const history = useHistory();
-  const location = useLocation();
-
-  useEffect(() => {
-    const token = window.localStorage.getItem("token");
-    token != null ? setLoggedIn(true) : setLoggedIn(false);
-  }, [location]);
-
-
-  const { image } = props;
+function Socials() {
   return (
-    <nav>
-      <div className="Menu">
-      <h1>LinkedIn</h1>
-      <img src="linkedin.png" alt=LinkedIn.">
-      <p>URL: linkedin.com/ecoocean</p>
-      <h2>Instagram</h2>
-      <img src="instagram.png" alt=Instagram.">
-      <p>IG handle: ecoocean</p>
-      </div>
-    </nav>
+    <div className="socials-container ">
+      <img className="social-img" src="./images/linkedin.png" alt="LinkedIn" />
+      <img className="social-img" src="./images/instagram.png" alt="Instagram" />
+    </div>
   );
 }
 export default Socials;
-
-
-<Navbar fixed="bottom" />
-
