ab6ce4d93f967481fb1ad477fd92929ac76764a7
Sasha Johnson <johnson.sashamarie@gmail.com>
2020-10-06 15:38:57 +0800

    Socials NavBar

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -1,10 +1,33 @@
+import React, { useState, useEffect } from "react";
+import { Link, useHistory, useLocation } from "react-router-dom";
+
+function Socials {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const history = useHistory();
+  const location = useLocation();
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    token != null ? setLoggedIn(true) : setLoggedIn(false);
+  }, [location]);
 
 
-
-
-<img src="linkedin.png" alt=LinkedIn.">
+  const { image } = props;
+  return (
+    <nav>
+      <div className="Menu">
       <h1>LinkedIn</h1>
+      <img src="linkedin.png" alt=LinkedIn.">
       <p>URL: linkedin.com/ecoocean</p>
-      <img src="instagram.png" alt=Instagram.">
       <h2>Instagram</h2>
+      <img src="instagram.png" alt=Instagram.">
       <p>IG handle: ecoocean</p>
+      </div>
+    </nav>
+  );
+}
+export default Socials;
+
+
+<Navbar fixed="bottom" />
+
