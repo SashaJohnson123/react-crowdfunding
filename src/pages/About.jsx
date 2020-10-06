88f07c978c2c74839299652c2a8212bdd2ae5a7f
Sasha Johnson <johnson.sashamarie@gmail.com>
2020-10-06 12:35:23 +0800

    Updated About Page styling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import "./About.css";
 
 function About() {
-  return <h1>About Us!</h1>;
+  return (
+    <div>
+      <h1>About Us</h1>
+      <a>
+        EcoOcean is a crowdfunding organisation that connects local projects on
+        the ground requiring funding, to those individuals or organisations that
+        are able to donate to a cause of their interest and concern. If you'd
+        like to register your local project, please do so and this will be
+        assessed internally prior to being added to this website. EcoOcean
+        primarily responds to significant environmental disasters at risk of
+        affecting wildlife that require immediate support. Please browse through
+        this website and make a difference to a local wildlife or animal
+        population today!
+      </a>
+    </div>
+  );
 }
 
 export default About;
