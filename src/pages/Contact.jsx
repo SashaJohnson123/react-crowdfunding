26978e889e33b7e8f55b9c9ae7548dac36e095e0
Sasha Johnson <johnson.sashamarie@gmail.com>
2020-10-06 14:17:23 +0800

    Updated Pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Contact() {
-  return <h1>Contact Us!</h1>;
-}
+  return (
+    <h1>Contact us!</h1>
+    <div>
+      <div class="form-item">
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name" required>
+      </div>
+      <div class="form-item">
+        <label for="mail">Email</label>
+        <input type="email" id="mail" name="email">
+      </div>
+      <div class="form-item">
+        <label for="message">Message</label>
+        <textarea id="message" name="message"></textarea>
+      </div>
+      <div class="form-item">
+        <button type="submit">Hit me</button>
+      </div>
+    </div>
 
 export default Contact;
--- src/pages/Subscribe.jsx
+++ src/pages/Subscribe.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 function Subscribe() {
-  return <h1>Subscribe Here!</h1>;
+  return (
+    <header>Subscribe here!</header>
+    <div>
+      <div>
+        <img src="linkedin.png" alt=LinkedIn.">
+      </div>
+      <div>
+          <h1>LinkedIn</h1>
+          <p>URL: linkedin.com/ecoocean</p>
+      </div>
+      <div>
+        <img src="instagram.png" alt=Instagram.">
+      </div>
+      <div>
+          <h2>Instagram</h2>
+          <p>IG handle: ecoocean</p>
+      </div>
+    </div> 
+  ); 
 }
 
 export default Subscribe;
