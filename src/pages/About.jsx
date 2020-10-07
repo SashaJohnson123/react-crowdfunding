6ddaf85568b1a8759dfaf3597a975bc04377199a
Sasha Johnson <johnson.sashamarie@gmail.com>
2020-10-07 10:41:35 +0800

    Updated Files

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,10 +10,12 @@ function About() {
         the ground requiring funding, to those individuals or organisations that
         are able to donate to a cause of their interest and concern. If you'd
         like to register your local project, please do so and this will be
-        assessed internally prior to being added to this website. EcoOcean
-        primarily responds to significant environmental disasters at risk of
-        affecting wildlife that require immediate support. Please browse through
-        this website and make a difference to a local wildlife or animal
+        assessed internally prior to being added to this website.
+      </p>
+      <p>
+        EcoOcean primarily responds to significant environmental disasters at
+        risk of affecting wildlife that require immediate support. Please browse
+        through this website and make a difference to a local wildlife or animal
         population today!
       </p>
     </div>
--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import Moment from "react-moment";
 import Pledges from "../components/Pledges/Pledges";
+import "./ProjectPage.css";
 
 function ProjectPage() {
   const [projectData, setProjectData] = useState({ pledge: [] });
@@ -15,6 +16,14 @@ function ProjectPage() {
     // send delete request to API using fetch
   };
 
+  const onEditClick = () => {
+    const shouldEdit = window.confirm(
+      "Are you sure you'd like to edit this project?"
+    );
+    if (!shouldEdit) return; // if user clicks cancel
+    // send edit request to API using fetch
+  };
+
   useEffect(() => {
     fetch(`${process.env.REACT_APP_API_URL}projects/${id}`)
       .then((results) => {
@@ -26,15 +35,15 @@ function ProjectPage() {
   }, []);
 
   return (
-    <div>
+    <div className="project-container">
       <h2>{projectData.title}</h2>
       <h3>{projectData.description}</h3>
-      <h3>
+      <h4>
         Created at:
-        <Moment format="DD/MM/YYYY">{projectData.created_date}</Moment>
-      </h3>
-      <h3>{`Status: ${projectData.is_open ? "Open" : "Closed"}`}</h3>
-      <h3>Pledges:</h3>
+        <Moment format="  DD/MM/YYYY">{projectData.created_date}</Moment>
+      </h4>
+      <h5>{`Status: ${projectData.is_open ? "Open" : "Closed"}`}</h5>
+      <h6>Pledges:</h6>
       <ul>
         {projectData.pledge.map((pledgeData, key) => {
           return (
@@ -46,7 +55,7 @@ function ProjectPage() {
         })}
       </ul>
       <Pledges projectData={projectData} />
-      {/* <button onClick={onEditClick}>Edit</button> */}
+      <button onClick={onEditClick}>Edit</button>
       <button onClick={onDeleteClick}>Delete</button>
     </div>
   );
